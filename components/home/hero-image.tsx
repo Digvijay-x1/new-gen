57292ef2146269c4ef459b8abd11f734aeb113db
0mBudsMan <buddhadevom@gmail.com>
2025-03-19 02:15:27 +0530

    init

---
+++ components/home/hero-image.tsx
@@ -0,0 +1,25 @@
+// Copyright Ayush Singh 2021,2022. All Rights Reserved.
+// Project: folio
+// Author contact: https://www.linkedin.com/in/alphaayush/
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+import * as React from "react";
+
+const HeroImage = () => (
+  <svg
+    width={1021}
+    height={650}
+    viewBox="0 0 1021 650"
+    className="w-full"
+    style={{
+      height: "auto",
+    }}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <h1>pop</h1>
+  </svg>
+);
+
+export default HeroImage;
