a3390ced8fdf0e6af5b0d63afaf8742300e7f5f7
Sudhanshu <sudhanshusah2005@gmail.com>
2025-10-03 00:06:01 +0530

    minor changes

--- components/e-summit/past-guests-section.tsx
+++ components/e-summit/past-guests-section.tsx
@@ -80,7 +80,7 @@ const PastQuestsSection = () => {
           className="mb-8 text-3xl font-bold text-center sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl text-primary sm:mb-10 md:mb-12 lg:mb-16"
           style={{ textShadow: '0 0 12px var(--color-primary)' }}
         >
-          Past Speakers
+          Past Guests
         </h2>
         <div className="grid grid-cols-1 gap-4 mx-auto xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 sm:gap-6 md:gap-8 max-w-7xl">
           {speakersData.map((speaker, index) => (
