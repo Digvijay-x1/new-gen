b8865d8dfc732231529b0f1b4f276270c5469f70
Sudhanshu <sudhanshusah2005@gmail.com>
2025-10-09 20:27:58 +0530

    Improved text

--- components/home/collaboration.tsx
+++ components/home/collaboration.tsx
@@ -117,14 +117,14 @@ const CollaborationSection = () => {
   return (
     <section className={COLLABORATION_STYLE.SECTION} ref={targetSection}>
       {renderSlidingText(
-        " User Interface Design  User Experience Design ",
+        " Entrepreneurship Cell  Innovation Programs ",
         "ui-left"
       )}
 
       {renderTitle()}
 
       {renderSlidingText(
-        " Frontend Development  Motion Graphics ",
+        " Startup Mentorship  Investor Networking ",
         "mt-6 md:mt-8 ui-right"
       )}
     </section>
